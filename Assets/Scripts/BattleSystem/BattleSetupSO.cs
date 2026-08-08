b163215cc8d569fd1846b124aef588ed2da4bf92
Seltu <sndm.snf22@uea.edu.br>
2026-08-07 23:15:05 -0400

    Add battle system animations and icons

---
+++ Assets/Scripts/BattleSystem/BattleSetupSO.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BattleSetupSO", menuName = "Legends/BattleSetupSO", order = 1)]
+public class BattleSetupSO : ScriptableObject
+{
+    public UnitSO playerUnit;
+    public List<UnitSO> randomUnitList;
+
+    public UnitSO RandomUnit()
+    {
+        return randomUnitList[Random.Range(0, randomUnitList.Count)];
+    }
+}
--- Assets/Scripts/BattleSystem/BattleUnit.cs
+++ Assets/Scripts/BattleSystem/BattleUnit.cs
@@ -6,9 +6,17 @@ using UnityEngine.UI;
 
 public class BattleUnit : MonoBehaviour
 {
+    [SerializeField] private Image unitImage;
+    [SerializeField] private Image shieldImage;
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private Slider hpSlider;
     [SerializeField] private TextMeshProUGUI hpText;
+    [SerializeField] private Animator unitAnimator;
+
+    [Header("Move Sprites")]
+    [SerializeField] private Sprite counterSprite;
+    [SerializeField] private Sprite defendSprite;
+
     private UnitSO _unitSO;
     private int _currentHp;
     private BattleMove _currentMove;
@@ -19,6 +27,7 @@ public class BattleUnit : MonoBehaviour
 
     public void SetHUD(UnitSO unit)
     {
+        unitImage.sprite = unit.unitSprite;
         nameText.text = unit.unitName;
         _unitSO = unit;
         _currentHp = _unitSO.maxHp;
@@ -29,6 +38,41 @@ public class BattleUnit : MonoBehaviour
         hpText.text = _currentHp + "/" + _unitSO.maxHp;
         hpSlider.normalizedValue = (float)_currentHp / _unitSO.maxHp;
     }
+
+    public void PlayMoveAnimation()
+    {
+        switch (_currentMove)
+        {
+            case BattleMove.ATTACK:
+                unitAnimator.SetTrigger("Attack");
+                break;
+
+            case BattleMove.SPECIALATTACK:
+                unitAnimator.SetTrigger("Special");
+                break;
+
+            case BattleMove.DEFEND:
+                unitAnimator.SetTrigger("Defense");
+                break;
+
+            case BattleMove.CHARGEUP:
+                unitAnimator.SetTrigger("Charge");
+                break;
+
+            case BattleMove.COUNTER:
+                unitAnimator.SetTrigger("Counter");
+                break;
+        }
+    }
+
+    public void SetShieldSprite(bool counter)
+    {
+        if (counter)
+            shieldImage.sprite = counterSprite;
+        else
+            shieldImage.sprite = defendSprite;
+    }
+
     public bool TakeDamage(int dmg)
     {
         _currentHp -= dmg;
